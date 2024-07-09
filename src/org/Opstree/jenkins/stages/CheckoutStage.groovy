c88c6e6035afb3090f91966eb2fa0938de2454d4
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:37:28 +0530

    Update CheckoutStage.groovy

--- src/org/Opstree/jenkins/stages/CheckoutStage.groovy
+++ src/org/Opstree/jenkins/stages/CheckoutStage.groovy
@@ -1,11 +1,7 @@
 package org.Opstree.jenkins.stages
 
-class CheckoutStage {
-    static void stage(Map params) {
-        stage('Checkout') {
-            steps {
-                git branch: params.GIT_BRANCH, url: params.GIT_URL
-            }
-        }
-    }
+def call(Map params) {
+    sh '''
+        git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
+    '''
 }
