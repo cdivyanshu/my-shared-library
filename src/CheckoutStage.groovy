c2959250a1dee6fedde748193466aa336cb1e5ea
Suryavanshiaditya1999 <137984887+Suryavanshiaditya1999@users.noreply.github.com>
2024-07-10 04:54:19 +0530

    comm

--- src/CheckoutStage.groovy
+++ src/CheckoutStage.groovy
@@ -1,5 +1,3 @@
-package org.Opstree.Jenkins.stages
-
 def call() {
     sh '''
         git clone "https://github.com/Naresh-boyini/employee-api.git"
--- vars/checkoutStage.groovy
+++ vars/checkoutStage.groovy
@@ -1,5 +1,3 @@
-import org.Opstree.Jenkins.stages.CheckoutStage
-
 def call() {
     new CheckoutStage().call()
 }
