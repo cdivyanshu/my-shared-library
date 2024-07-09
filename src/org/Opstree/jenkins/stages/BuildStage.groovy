b4ece11ee08551bdfab301ee77a097701910351a
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:36:50 +0530

    Update BuildStage.groovy

--- src/org/Opstree/jenkins/stages/BuildStage.groovy
+++ src/org/Opstree/jenkins/stages/BuildStage.groovy
@@ -1,15 +1,9 @@
 package org.Opstree.jenkins.stages
 
-class BuildStage {
-    static void stage() {
-        stage('Build') {
-            steps {
-                sh '''
-                go mod tidy
-                go mod download
-                go build -o employee-api .
-                '''
-            }
-        }
-    }
+def call(Map params) {
+    sh '''
+        go mod tidy
+        go mod download
+        go build -o employee-api .
+    '''
 }
