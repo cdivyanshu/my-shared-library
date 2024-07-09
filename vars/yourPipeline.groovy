53728f64aec1c438891f3e7460f74a5dbe2e24b7
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:57:37 +0530

    Update yourPipeline.groovy

--- vars/yourPipeline.groovy
+++ vars/yourPipeline.groovy
@@ -13,4 +13,69 @@ def call(Map params) {
             string(name: 'GIT_URL', defaultValue: 'https://github.com/Naresh-boyini/employee-api.git', description: 'Git repository URL')
             string(name: 'SCANNER_TOOL_NAME', defaultValue: 'sonar', description: 'Name of the SonarQube Scanner tool as configured in Jenkins')
             string(name: 'SONARQUBE_ENV_NAME', defaultValue: 'sonar-kumar', description: 'SonarQube environment name')
-            string(name: 'ZAP_PORT', defaultValue: '8080', description:
+            string(name: 'ZAP_PORT', defaultValue: '8080', description: 'Port for ZAP to use')
+            string(name: 'ZAP_URL', defaultValue: 'http://34.173.102.226/api/v1/employee/health', description: 'URL for ZAP to scan')
+        }
+
+        stages {
+            stage('Code Compilation') {
+                steps {
+                    script {
+                        CompileCode.call(
+                            GIT_BRANCH: params.GIT_BRANCH,
+                            GIT_URL: params.GIT_URL
+                        )
+                    }
+                }
+            }
+
+            stage('Static Code Analysis') {
+                steps {
+                    script {
+                        StaticCodeAnalysis.call(
+                            SCANNER_TOOL_NAME: params.SCANNER_TOOL_NAME,
+                            SONARQUBE_ENV_NAME: params.SONARQUBE_ENV_NAME
+                        )
+                    }
+                }
+            }
+
+            stage('Bugs Analysis') {
+                steps {
+                    script {
+                        BugsAnalysis.call()
+                    }
+                }
+            }
+
+            stage('Dependency Scanning') {
+                steps {
+                    script {
+                        DependencyScanning.call()
+                    }
+                }
+            }
+
+            stage('Dynamic Application Security Testing (DAST)') {
+                steps {
+                    script {
+                        Dast.call(
+                            ZAP_PORT: params.ZAP_PORT,
+                            ZAP_URL: params.ZAP_URL
+                        )
+                    }
+                }
+            }
+        }
+
+        post {
+            success {
+                echo 'Build successful!'
+            }
+
+            failure {
+                echo 'Build failed!'
+            }
+        }
+    }
+}
