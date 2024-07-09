b372147ac80af8cdf3c91ecb3ca2920ed9bec370
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:37:53 +0530

    Update DependencyScanStage.groovy

--- src/org/Opstree/jenkins/stages/DependencyScanStage.groovy
+++ src/org/Opstree/jenkins/stages/DependencyScanStage.groovy
@@ -1,13 +1,9 @@
 package org.Opstree.jenkins.stages
 
-class DependencyScanStage {
-    static void stage(Map params) {
-        stage('Dependency Scan') {
-            steps {
-                // Run OWASP Dependency-Check
-                dependencyCheck additionalArguments: '--scan . --format ALL', odcInstallation: params.DEPENDENCY_TOOL_NAME
-            }
-        }
-    }
+def call(Map params) {
+    sh '''
+        dependencyCheck additionalArguments: '--scan . --format ALL', odcInstallation: "${params.DEPENDENCY_TOOL_NAME}"
+    '''
 }
+
 
