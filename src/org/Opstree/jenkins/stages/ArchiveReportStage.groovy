455f5b0e37e861dbe5cf4c49435aefc00de37477
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:36:15 +0530

    Update ArchiveReportStage.groovy

--- src/org/Opstree/jenkins/stages/ArchiveReportStage.groovy
+++ src/org/Opstree/jenkins/stages/ArchiveReportStage.groovy
@@ -1,24 +1,19 @@
 package org.Opstree.jenkins.stages
 
-class ArchiveReportStage {
-    static void stage() {
-        stage('Archive Report') {
-            steps {
-                // Archive the unit test results
-                archiveArtifacts artifacts: 'unit-test-results.xml', allowEmptyArchive: true
+def call(Map params) {
+    sh '''
+        echo "Archiving reports..."
+        archiveArtifacts artifacts: 'unit-test-results.xml', allowEmptyArchive: true
 
-                echo "Publishing Reports"
-                // Publish OWASP Dependency-Check report in HTML format
-                publishHTML([
-                    allowMissing: false,
-                    alwaysLinkToLastBuild: true,
-                    keepAll: true,
-                    reportDir: '.',
-                    reportFiles: 'dependency-check-report.html',
-                    reportName: 'Dependency Check Report',
-                    reportTitles: ''
-                ])
-            }
-        }
-    }
+        echo "Publishing Reports"
+        publishHTML([
+            allowMissing: false,
+            alwaysLinkToLastBuild: true,
+            keepAll: true,
+            reportDir: '.',
+            reportFiles: 'dependency-check-report.html',
+            reportName: 'Dependency Check Report',
+            reportTitles: ''
+        ])
+    '''
 }
