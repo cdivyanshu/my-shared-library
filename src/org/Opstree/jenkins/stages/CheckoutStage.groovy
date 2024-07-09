eb763c028669ece59f17c328cae42ea05fb60917
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 04:04:45 +0530

    Update CheckoutStage.groovy

--- src/org/Opstree/jenkins/stages/CheckoutStage.groovy
+++ src/org/Opstree/jenkins/stages/CheckoutStage.groovy
@@ -2,6 +2,6 @@ package org.Opstree.jenkins.stages
 
 def call(Map params) {
     sh '''
-        git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
+        git clone "https://github.com/Naresh-boyini/employee-api.git"
     '''
 }
