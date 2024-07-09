f9aa9ed86fac1dca4fb0f27b5de041bf26c5ca86
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:40:04 +0530

    Update StaticCodeAnalysis.groovy

--- src/org/Opstree/jenkins/stages/StaticCodeAnalysis.groovy
+++ src/org/Opstree/jenkins/stages/StaticCodeAnalysis.groovy
@@ -1,30 +1,12 @@
-package org.Opstree.jenkins.stages
+package org.Optree.jenkins.stages
 
-def call(Map params) {
-    pipeline {
-        agent any
-
-        environment {
-            SCANNER_HOME = tool "${params.SCANNER_TOOL_NAME}"
-        }
-
-        stages {
-            stage('SonarQube Analysis') {
-                steps {
-                    withSonarQubeEnv("${params.SONARQUBE_ENV_NAME}") {
-                        sh "${SCANNER_HOME}/bin/sonar-scanner"
-                    }
+class StaticCodeAnalysis {
+    static void call(Map params) {
+        stage('SonarQube Analysis') {
+            steps {
+                withSonarQubeEnv("${params.SONARQUBE_ENV_NAME}") {
+                    sh "${SCANNER_HOME}/bin/sonar-scanner"
                 }
-            }
-        }
-
-        post {
-            success {
-                echo 'Static code analysis successful!'
-            }
-
-            failure {
-                echo 'Static code analysis failed!'
             }
         }
     }
