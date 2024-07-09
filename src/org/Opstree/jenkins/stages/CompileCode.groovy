65572a227070d3d196343ddd83c19dbc233b1ac9
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:06:03 +0530

    Created CompileCode.groovy

---
+++ src/org/Opstree/jenkins/stages/CompileCode.groovy
@@ -0,0 +1,48 @@
+package org.yourorganization.jenkins.stages
+
+def call(Map params) {
+    pipeline {
+        agent any
+
+        environment {
+            GO_VERSION = '1.18'
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
+                }
+            }
+
+            stage('Modify go.mod') {
+                steps {
+                    sh '''
+                    export GO111MODULE=on
+                    sed -i 's/go 1.20/go 1.18/g' go.mod
+                    '''
+                }
+            }
+
+            stage('Build') {
+                steps {
+                    sh '''
+                    go mod tidy
+                    go mod download
+                    go build -o employee-api .
+                    '''
+                }
+            }
+        }
+
+        post {
+            success {
+                echo 'Compilation successful!'
+            }
+
+            failure {
+                echo 'Compilation failed!'
+            }
+        }
+    }
+}
