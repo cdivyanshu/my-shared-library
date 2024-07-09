2011bbfa07eaf1aa7d93fdcacfd9652a08220b1c
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:39:40 +0530

    Update CompileCode.groovy

--- src/org/Opstree/jenkins/stages/CompileCode.groovy
+++ src/org/Opstree/jenkins/stages/CompileCode.groovy
@@ -1,47 +1,29 @@
-package org.Opstree.jenkins.stages
+package org.Optree.jenkins.stages
 
-def call(Map params) {
-    pipeline {
-        agent any
-
-        environment {
-            GO_VERSION = '1.18'
-        }
-
-        stages {
-            stage('Checkout') {
-                steps {
-                    git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
-                }
-            }
-
-            stage('Modify go.mod') {
-                steps {
-                    sh '''
-                    export GO111MODULE=on
-                    sed -i 's/go 1.20/go 1.18/g' go.mod
-                    '''
-                }
-            }
-
-            stage('Build') {
-                steps {
-                    sh '''
-                    go mod tidy
-                    go mod download
-                    go build -o employee-api .
-                    '''
-                }
+class CompileCode {
+    static void call(Map params) {
+        stage('Checkout') {
+            steps {
+                git branch: "${params.GIT_BRANCH}", url: "${params.GIT_URL}"
             }
         }
 
-        post {
-            success {
-                echo 'Compilation successful!'
+        stage('Modify go.mod') {
+            steps {
+                sh '''
+                export GO111MODULE=on
+                sed -i 's/go 1.20/go 1.18/g' go.mod
+                '''
             }
+        }
 
-            failure {
-                echo 'Compilation failed!'
+        stage('Build') {
+            steps {
+                sh '''
+                go mod tidy
+                go mod download
+                go build -o employee-api .
+                '''
             }
         }
     }
