bfa14517c07cebe8edfc8769ee41ad306ed13032
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:39:03 +0530

    Update SonarQubeAnalysisStage.groovy

--- src/org/Opstree/jenkins/stages/SonarQubeAnalysisStage.groovy
+++ src/org/Opstree/jenkins/stages/SonarQubeAnalysisStage.groovy
@@ -1,13 +1,9 @@
 package org.Opstree.jenkins.stages
 
-class SonarQubeAnalysisStage {
-    static void stage(Map params) {
-        stage('SonarQube Analysis') {
-            steps {
-                withSonarQubeEnv(params.SONARQUBE_ENV_NAME) {
-                    sh "${params.SCANNER_HOME}/bin/sonar-scanner"
-                }
-            }
+def call(Map params) {
+    sh '''
+        withSonarQubeEnv("${params.SONARQUBE_ENV_NAME}") {
+            sh "${SCANNER_HOME}/bin/sonar-scanner"
         }
-    }
+    '''
 }
