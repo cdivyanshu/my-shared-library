de0bbc294fd8d77a9b2f5d013ac45f0dcd1ca87a
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:39:40 +0530

    Update UnitTestStage.groovy

--- src/org/Opstree/jenkins/stages/UnitTestStage.groovy
+++ src/org/Opstree/jenkins/stages/UnitTestStage.groovy
@@ -1,11 +1,7 @@
 package org.Opstree.jenkins.stages
 
-class UnitTestStage {
-    static void stage() {
-        stage('Unit Test') {
-            steps {
-                sh 'go test ./... -v | tee unit-test-results.xml'
-            }
-        }
-    }
+def call(Map params) {
+    sh '''
+        go test ./... -v | tee unit-test-results.xml
+    '''
 }
