2740816680da5805ca65e46bcf0d384b885b0e0f
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:08:46 +0530

    Create DependencyScanning.groovy

---
+++ src/org/Opstree/jenkins/stages/DependencyScanning.groovy
@@ -0,0 +1,25 @@
+package org.yourorganization.jenkins.stages
+
+def call() {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Dependency Scan') {
+                steps {
+                    dependencyCheck additionalArguments: '--scan . --format ALL', odcInstallation: 'OWASP Dependency-Check Vulnerabilities'
+                }
+            }
+        }
+
+        post {
+            success {
+                echo 'Dependency scanning successful!'
+            }
+
+            failure {
+                echo 'Dependency scanning failed!'
+            }
+        }
+    }
+}
