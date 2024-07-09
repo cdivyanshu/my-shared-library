325dd7dbc702365a534cb931828833768a5e81a4
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-10 03:38:32 +0530

    Update ModifyGoModStage.groovy

--- src/org/Opstree/jenkins/stages/ModifyGoModStage.groovy
+++ src/org/Opstree/jenkins/stages/ModifyGoModStage.groovy
@@ -1,14 +1,8 @@
 package org.Opstree.jenkins.stages
 
-class ModifyGoModStage {
-    static void stage() {
-        stage('Modify go.mod') {
-            steps {
-                sh '''
-                export GO111MODULE=on
-                sed -i 's/go 1.20/go 1.18/g' go.mod
-                '''
-            }
-        }
-    }
+def call(Map params) {
+    sh '''
+        export GO111MODULE=on
+        sed -i 's/go 1.20/go 1.18/g' go.mod
+    '''
 }
