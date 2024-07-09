2dc506bf8448090102e652660e4de53ff0f725c3
Divyanshu Chaudhary <160396963+cdivyanshu@users.noreply.github.com>
2024-07-09 17:40:31 +0530

    Update BugsAnalysis.groovy

--- src/org/Opstree/jenkins/stages/BugsAnalysis.groovy
+++ src/org/Opstree/jenkins/stages/BugsAnalysis.groovy
@@ -1,24 +1,10 @@
-package org.Opstree.jenkins.stages
+package org.Optree.jenkins.stages
 
-def call() {
-    pipeline {
-        agent any
-
-        stages {
-            stage('Unit Test') {
-                steps {
-                    sh 'go test ./... -v | tee unit-test-results.xml'
-                }
-            }
-        }
-
-        post {
-            success {
-                echo 'Bugs analysis successful!'
-            }
-
-            failure {
-                echo 'Bugs analysis failed!'
+class BugsAnalysis {
+    static void call() {
+        stage('Unit Test') {
+            steps {
+                sh 'go test ./... -v | tee unit-test-results.xml'
             }
         }
     }
